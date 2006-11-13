74793205645b5a29465e32878cea6158af0220bc
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-11-13 10:24:07 +0100

    first import
    darcs-hash:20061113092407-2fc9d-bc75c45c7401da27034b3c1d6022257cdd125bf7.gz

--- check.cxx
+++ check.cxx
@@ -3,13 +3,14 @@
 #include "itkCommand.h"
 #include "itkSimpleFilterWatcher.h"
 
-#include "itkImageFilter.h"
-
+#include "itkRobustAutomaticThresholdImageFilter.h"
+#include "itkMorphologicalGradientImageFilter.h"
+#include "itkBinaryBallStructuringElement.h"
 
 int main(int argc, char * argv[])
 {
 
-  if( argc !=  )
+  if( argc != 3 )
     {
     std::cerr << "usage: " << argv[0] << " " << std::endl;
     // std::cerr << "  : " << std::endl;
@@ -18,22 +19,41 @@ int main(int argc, char * argv[])
 
   const int dim = 2;
   
-  typedef unsigned char PType;
+  typedef unsigned short PType;
   typedef itk::Image< PType, dim > IType;
+
+  typedef PType RPType;
+  typedef itk::Image< RPType, dim > RIType;
 
   typedef itk::ImageFileReader< IType > ReaderType;
   ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName( argv[1] );
-
-  typedef itk::ImageFilter< IType, IType > FilterType;
+  
+  typedef itk::BinaryBallStructuringElement< bool, dim > KernelType;
+  KernelType kernel;
+  KernelType::RadiusType rad;
+  rad.Fill(2);
+  kernel.SetRadius( rad );
+  kernel.CreateStructuringElement();
+  
+  typedef itk::MorphologicalGradientImageFilter< IType, RIType, KernelType > GradientType;
+  GradientType::Pointer gradient = GradientType::New();
+  gradient->SetInput( reader->GetOutput() );
+  gradient->SetKernel( kernel );
+//  gradient->Update();
+//  std::cout << gradient->GetOutput()->GetSpacing() << std::endl;
+  
+  typedef itk::RobustAutomaticThresholdImageFilter< IType, RIType > FilterType;
   FilterType::Pointer filter = FilterType::New();
   filter->SetInput( reader->GetOutput() );
+  filter->SetGradientImage( gradient->GetOutput() );
 
   itk::SimpleFilterWatcher watcher(filter, "filter");
 
   typedef itk::ImageFileWriter< IType > WriterType;
   WriterType::Pointer writer = WriterType::New();
-  writer->SetInput( filter->GetOutput() );
+  writer->SetInput( gradient->GetOutput() );
+//  writer->SetInput( filter->GetOutput() );
   writer->SetFileName( argv[2] );
   writer->Update();
 
